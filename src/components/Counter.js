d30e6a235749564fba0443af32df8e4658e37ec9
DESKTOP-AS9LOOG\shark <seonjae1003@naver.com>
2024-12-04 14:43:03 +0900

    작업에 페이로드 연결

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,9 +2,13 @@ import { useSelector, useDispatch } from "react-redux";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
+  const increaseAmount = 5;
   const counter = useSelector((state) => state.count);
   const dispatch = useDispatch();
 
+  const increaseHandler = (amount) => {
+    dispatch({ type: "increase", amount: amount });
+  };
   const increHandler = () => {
     dispatch({ type: "increment" });
   };
@@ -20,6 +24,9 @@ const Counter = () => {
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={increHandler}>Increment</button>
+        <button onClick={() => increaseHandler(increaseAmount)}>
+          Increase by {increaseAmount}
+        </button>
         <button onClick={decreHandler}>Decrement</button>
         <button onClick={toggleCounterHandler}>Toggle Counter</button>
       </div>
--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,11 @@ const reducer = (state = { count: 0 }, action) => {
   if (action.type === "increment") return { count: state.count + 1 };
   if (action.type === "decrement") return { count: state.count - 1 };
 
+  // 하드코딩 (지양)
+  // if (action.type === "increase") return { count: state.count + 5 };
+  // action.[key] 로 동적 연결
+  if (action.type === "increase") return { count: state.count + action.amount };
+
   return state;
 };
 
