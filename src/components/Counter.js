97b05bc53f5579da5146354526566ca3bf211879
DESKTOP-AS9LOOG\shark <seonjae1003@naver.com>
2024-12-04 14:02:38 +0900

    생성한 redux를 함수형 컴포넌트에 사용

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,28 @@
-import classes from './Counter.module.css';
+import { useSelector, useDispatch } from "react-redux";
+import classes from "./Counter.module.css";
 
 const Counter = () => {
+  const counter = useSelector((state) => state.count);
+  const dispatch = useDispatch();
+
+  const increHandler = () => {
+    dispatch({ type: "increment" });
+  };
+  const decreHandler = () => {
+    dispatch({ type: "decrement" });
+  };
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={increHandler}>Increment</button>
+        <button onClick={decreHandler}>Decrement</button>
+        <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      </div>
     </main>
   );
 };
