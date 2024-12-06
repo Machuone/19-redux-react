49108d3a5968e00fce5ffa5f592db05c5df033a7
DESKTOP-AS9LOOG\shark <seonjae1003@naver.com>
2024-12-06 11:44:06 +0900

    리덕스 툴킷을 활용하여 코드 개선

--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,33 @@
-import { createStore } from "redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-// 여러 state props 존재 시 타겟 props를 제외한 props들도 값 설정해줘야 함을 주의
-const reducer = (state = { count: 0, showing: true }, action) => {
-  if (action.type === "increment")
-    return { count: state.count + 1, showing: state.showing };
-  if (action.type === "decrement")
-    return { count: state.count - 1, showing: state.showing };
+const initialState = { count: 0, showCounter: true };
 
-  // 하드코딩 (지양)
-  // if (action.type === "increase") return { count: state.count + 5 };
-  // action.[key] 로 동적 연결
-  if (action.type === "increase")
-    return { count: state.count + action.amount, showing: state.showing };
+const countSlice = createSlice({
+  name: "count",
+  initialState,
+  reducers: {
+    increment(state) {
+      // 리덕스 툴킷에서 immer라는 패키지를 통해
+      // 기존 상태를 변경할 수 없게 해줌
+      // 따라서 기존 상태를 변경하는 코드를 작성해도
+      // 내부적으로 변경되지 않게 설정해줌
+      state.count++;
+    },
+    decrement(state) {
+      state.count--;
+    },
+    increase(state, action) {
+      state.count = state.count + action.amount;
+    },
+    toggle(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
-  if (action.type === "toggle")
-    return { count: state.count, showing: !state.showing };
-
-  return state;
-};
-
-const store = createStore(reducer);
+// configureStore를 이용하여 모든 리듀서를 하나의 큰 리듀서로 병합
+const store = configureStore({
+  reducer: { counter: countSlice.reducer },
+});
 
 export default store;
